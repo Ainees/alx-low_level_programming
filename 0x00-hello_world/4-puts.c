0914a84a469c140e0dd3569c63e7db4a4fd1498b
Ainees <Sauveur2303@gmail.com>
2022-09-08 18:20:41 -0700

    puts number 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - printing a string using the put functions
- * 
+ *
  * this program prints a message
  * Return: 0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
