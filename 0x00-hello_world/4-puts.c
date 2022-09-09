84147c8662060b885e85913ae6a7c987c2d421dc
Ainees <Sauveur2303@gmail.com>
2022-09-08 17:48:22 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
-
+#include <string.h>
 /**
- *main- using the put function to printing a messaging
- *
- * Return: (0)
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	char str1[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str1);
+	return (0);
 }
