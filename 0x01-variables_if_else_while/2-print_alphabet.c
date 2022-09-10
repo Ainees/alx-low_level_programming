51e1f132726950fc0b39741aae207b1223a85abb
Ainees <Sauveur2303@gmail.com>
2022-09-10 12:46:08 -0700

    print letter

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,12 +5,17 @@
  * Description: prints the alphabet in lowercase with a newline
  * Return: Always 0 (Success)
  */
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 int main(void)
 {
-	char ch;
-	for (ch = 'a' ; ch <= 'z'; ch++)
+	char l;
+
+	for (l = 'a' ; l <= 'z'; l++)
 {
-		putchar(ch);
+		putchar(l);
 }
 putchar('\n');
 return (0);
