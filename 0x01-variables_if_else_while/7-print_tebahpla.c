f3b017acb9910bb88a73ec6ebf2ad99058ac30f4
Ainees <sauveur2303@gmail.com>
2022-09-12 18:48:29 -0700

    program that prints all the numbers of base 16 in lowercase

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
+
 /**
- * *main - Start point
- *
- * Description: program that prints the lowercase alphabet in reverse
- *
- * Return: 0
+ * main - print if the number is postive, zero, or negative
+ * Description: a program that prints the lowercase alphabet in reverse
+ * Return: Always 0 Success
  */
+
 int main(void)
 {
 	char c;
-	
+
 	for (c = 'z'; c >= 'a'; c--)
 	{
 		putchar(c);
