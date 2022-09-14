451dfb32ef64753597e9c6b42b55587f95c38f12
Ainees <sauveur2303@gmail.com>
2022-09-14 14:38:18 -0700

    pushing task 7 and 8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,18 +3,19 @@
 /**
  * _abs - a function that prints the last digit of a number.
  * Description: This program will prints the last digit of a number.
+ * 
  * @n: The number at present
  * Return: 0
  */
 int print_last_digit(int n)
 
 {
-	int last_digit = n % 10;
-	
-	if (last_digit < 0)
-	{
-		last_digit *= -1;
-	}
+int last_digit = n % 10;
+
+if (last_digit < 0)
+{
+last_digit *= -1;
+}
 _putchar(last_digit + '0');
 return (last_digit);
 }
