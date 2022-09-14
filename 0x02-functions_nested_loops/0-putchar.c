abcae7266785b6a96ecfb7df99d8b838e176a377
Ainees <sauveur2303@gmail.com>
2022-09-14 11:16:00 -0700

    push putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
  * main - Starting point
  * Description: This program prints _putchar, followed by a new line.
- * Return: Always 0
+ * Return: Always (0)
  */
-int main (void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
