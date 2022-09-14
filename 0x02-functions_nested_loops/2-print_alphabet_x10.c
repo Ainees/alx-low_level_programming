429800278938fb1af6859f01b98b9d32a2055a4b
Ainees <sauveur2303@gmail.com>
2022-09-14 11:56:12 -0700

    print 10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+nclude "main.h"
+
+/**
+ * print_alphabet_x10 - function to print abc 10 times
+ * Definition: A program prints 10 times the alphabet, in lowercase.
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char p;
+	char k;
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; k <= 'z'; p++)
+		{
+			_putchar(p);
+		}
+		_putchar('\n');
+	}
+}
