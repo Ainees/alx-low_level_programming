1a7d8b1321bca31c74e018d63eb0f9db1cb0a2e8
Ainees <sauveur2303@gmail.com>
2022-09-14 11:11:32 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Starting point
+ * Description: This program prints _putchar, followed by a new line.
+ * Return: Always 0
+ */
+int main (void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	_putchar('\n');
+	
+	return 0;
+}
