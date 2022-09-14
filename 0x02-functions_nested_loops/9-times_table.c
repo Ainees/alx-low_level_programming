2e25f96628748161e188fff2d2085f18ef677323
Ainees <sauveur2303@gmail.com>
2022-09-14 15:04:36 -0700

    commit task 8

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,9 @@
-nclude "main.h"
-
-
-
+#include "main.h"
 /**
  *
- *  *times_table - Prints the 9 times table, starting with 0.
+ * times_table - a function that prints the 9 times table, starting with 0.
  *
- *   */
+ */
 
 void times_table(void)
 {
