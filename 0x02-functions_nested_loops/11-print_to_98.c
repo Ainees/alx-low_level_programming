2fa86ee0498372c5bdff9cc1cebbb2158a063e52
Ainees <sauveur2303@gmail.com>
2022-09-15 10:26:26 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * print_to_98 - prints all natural numbers from input to 98,in order separated by a comma.
- * Description: function that prints all natural numbers from n to 98, followed by a new line.
+ * print_to_98 - prints all natural numbers from input to 98.
+ * Description: function that prints all natural numbers from n to 98.
  * @n: The number to begin counting at.
  */
-void print_to_98 (int n)
+void print_to_98(int n)
 {
 	if (n >= 98)
 	{
@@ -14,8 +14,8 @@ void print_to_98 (int n)
 	}
 	else
 	{
-		while(n < 98)
+		while (n < 98)
 			printf("%d, ", n++);
-		printf("%d\n", n);	
+		printf("%d\n", n);
 	}
 }
