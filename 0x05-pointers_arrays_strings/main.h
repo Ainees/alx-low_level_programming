98dea8ee5b2f4b02554ca41e698871fc4c509e5f
Ainees <sauveur2303@gmail.com>
2022-09-19 17:15:02 -0700

    0x05

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * void prototypes()
+ * int prototypes()
+ * Description: pointers and array
+ */
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -8,3 +17,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+
+#endif /*MAIN_H*/
