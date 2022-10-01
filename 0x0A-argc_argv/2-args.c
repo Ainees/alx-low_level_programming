56f1ebcf270688e1ce3653d7eb0f7d5eb43b5baa
Ainees <sauveur2303@gmail.com>
2022-09-30 17:02:09 -0700

    completed

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,15 +2,23 @@
 #include "main.h"
 
 /**
- * main - print the name of the program@argc: Count arguments@argv: Arguments
+ * main - Print the name of the program
  * @argc: Count arguments
  * @argv: Arguments
  *
  * Return: Always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
-	printf("%i\n", argc - 1);
-	(void)argv;
+	int count = 0;
+
+	if (argc > 0)
+	{
+		while (count < argc)
+	{
+	printf("%s\n", argv[count]);
 	return (0);
+}
+}
 }
